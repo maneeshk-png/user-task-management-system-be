7ca7631fb1cbce361d2400622200ae54dc5ab729
maneesh <maneeshk@unicourt.com>
2026-02-07 18:10:27 +0530

    feat(auth): implement basic POST /auth/login authentication

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { UsersRepository } from '../users/users.repository';
 import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from 'bcrypt';
 import { PasswordService } from './password.service';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -37,5 +38,31 @@ export class AuthService {
       message: 'User registered successfully',
       user: savedUser,
     };
+    }
+
+    //Login User
+    async login(dto:LoginDto){
+      // Find user by username
+      const user = await this.usersRepo.findByUsername(dto.username);
+      
+      // If user not found, throw error
+      if (!user) {
+        throw new ConflictException('Invalid username or password');
+      }
+
+      // Compare passwords
+      const isPasswordValid = await this.passwordService.comparePassword(dto.password, user.password);
+      
+      // If password is invalid, throw error
+      if (!isPasswordValid) {
+        throw new ConflictException('Invalid username or password');
+      }
+
+      // If login successful, return user data (excluding password)
+      const { password, ...userWithoutPassword } = user;
+      return {
+        message: 'Login successful',
+        user: userWithoutPassword,
+      };
     }
 }
